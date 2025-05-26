ad812c4e5d0e0831a3f5b1fbb4f2332e737a4f5a
Juan Felipe Rodriguez Amador <149622001+Phel1405@users.noreply.github.com>
2025-05-26 18:58:29 -0500

    Update procesos.c

--- UtilsPdispersa/procesos.c
+++ UtilsPdispersa/procesos.c
@@ -11,13 +11,17 @@
 
 #include "procesos.h"
 
+/* Cuenta los elementos no cero en un segmento
+   de matriz */
 int contarNoCero(DatosProcesos arg) {
     DatosProcesos datos = arg;
-    int contador = 0;
+    int contador = 0; // Variable local para almacenar la cantidad de elementos no ceros
+
+    // Recorrido del segmento de matriz asignado
     for (int i = datos.filaInicial; i < datos.filaFinal; i++) {
         for (int j = 0; j < datos.columnas; j++) {
             if (datos.matriz[i][j] != 0) {
-                contador++;
+                contador++; // Aumenta la variable local
             }
         }
     }
@@ -25,14 +29,19 @@ int contarNoCero(DatosProcesos arg) {
     return contador;
 }
 
+/* Función principal ejecutada por cada proceso hijo */
 void funcProceso(DatosProcesos arg)
 {
     int cuenta = contarNoCero(arg);
+
+    // Comunicación de resultados
     if(cuenta<=254)
     {
+        // Resultado pequeño: usar código de salida directamente
         exit(cuenta);
     }
     else{
+        // Resultado grande: usar archivo temporal
         char filename[256];
         sprintf(filename, ARCHIVO_PATRON, getpid());
         FILE *file = fopen(filename, "w");
@@ -41,6 +50,7 @@ void funcProceso(DatosProcesos arg)
             fprintf(file, "%d", cuenta);
             fclose(file);
         }
+        // Código especial para indicar que el resultado está en archivo
         exit(255);
     }
 }
